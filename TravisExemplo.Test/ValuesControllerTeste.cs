3039c52efdfb7fc2ea5b3792ec6389bcc261981f
Nicolas Sylverio <nicolas_sylveriopereira@hotmail.com>
2019-03-14 21:54:14 -0300

    Tentativa Adicionar Coverage

---
+++ TravisExemplo.Test/ValuesControllerTeste.cs
@@ -0,0 +1,21 @@
+﻿using TravisExemplo.WebApi.Controllers;
+using Xunit;
+
+namespace TravisExemplo.Test
+{
+    public class ValuesControllerTeste
+    {
+        private readonly ValuesController valuesController;
+
+        public ValuesControllerTeste()
+        {
+            valuesController = new ValuesController();
+        }
+
+        [Fact]
+        public void ValuesController()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
